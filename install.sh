6c858275b8bad9ff69ef160121c856ba38229e3a
oryxfea <oryxfea@users.noreply.github.com>
2021-05-30 03:04:00 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,23 @@
 
 sudo apt install -y less
 sudo apt install -y gdb
+sudo apt install -y zsh
+
+sudo apt install -y python3
+sudo apt install -y python3-pip
+pip3 install powerline-status
+
+sudo apt install -y docker.io
+sudo apt install -y docker-compose
+
+sudo apt install -y nodejs
+sudo apt install -y npm
+sudo npm install -g yarn
+
+ln -s ~/.dotfiles/.zshrc ~
+ln -s ~/.dotfiles/.tmux.conf ~
+ln -s ~/.dotfiles/.gitconfig ~
+ln -s ~/.dotfiles/.gitignore ~
 
 pushd $HOME
 
--- tmux_script.sh
+++ tmux_script.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
-# Download from https://gist.github.com/febrianrendak/9578240
+# Build based on https://gist.github.com/febrianrendak/9578240
 
 function tmux_web {
  SESSION_NAME=$1
- #echo "Create.."
  tmux new -s $SESSION_NAME -n editor -d
- #echo "Creat first pane (editor).."
- #tmux send-keys -t $SESSION_NAME 'vim' C-m
- #echo "Run vim in first pane.."
- #tmux new-window -n console -t $SESSION_NAME
- #echo "Create second pane (console).."
- #tmux split-window -v -t $SESSION_NAME:2
- #echo "Split second pane.."
- #tmux new-window -n server -t $SESSION_NAME
- #echo "Create third pane (server).."
- #tmux select-window -t $SESSION_NAME:2
  tmux attach -t $SESSION_NAME
- #echo "Start tmux session $SESSION_NAME."
 }
 
 #new session base name is current directory name
@@ -29,10 +17,8 @@ then
   echo "Session not found. Create Session '$DIR_NAME'."
   tmux_web $DIR_NAME
 else
-  #random string as new session suffix
-  RAND_NUMB=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 2 | head -n 1)
-  echo "Session found. Create session with name '$DIR_NAME$RAND_NUMB'"
-  tmux_web "$DIR_NAME$RAND_NUMB"
+  echo "Session found. Attach to it"
+  tmux attach -t $DIR_NAME
 fi
 
 echo "Start tmux session $DIR_NAME."
